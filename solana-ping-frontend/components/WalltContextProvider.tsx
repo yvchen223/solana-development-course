76fa5ae8b7ebd1522bc029b69e27d15e87266308
yvchen <yvchen223@gmail.com>
2024-02-26 15:03:05 +0000

    interact with wallets

---
+++ solana-ping-frontend/components/WalltContextProvider.tsx
@@ -0,0 +1,20 @@
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { FC, ReactNode, useMemo } from "react";
+import * as web3 from "@solana/web3.js";
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+    const endpoint = web3.clusterApiUrl("devnet");
+    const wallets = useMemo(() => [], []);
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets}>
+                <WalletModalProvider>{children}</WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
+
+export default WalletContextProvider;
---
+++ solana-ping-frontend/pages/index.tsx
@@ -0,0 +1,29 @@
+import { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+import { AppBar } from '../components/AppBar'
+import Head from 'next/head'
+import { PingButton } from '../components/PingButton'
+import WalletContextProvider from '../components/WalltContextProvider'
+
+const Home: NextPage = (props) => {
+
+  return (
+    <div className={styles.App}>
+      <Head>
+        <title>Wallet-Adapter Example</title>
+        <meta
+          name="description"
+          content="Wallet-Adapter Example"
+        />
+      </Head>
+      <WalletContextProvider>
+        <AppBar />
+        <div className={styles.AppBody}>
+          <PingButton/>
+        </div>
+      </WalletContextProvider>
+    </div>
+  );
+}
+
+export default Home;
